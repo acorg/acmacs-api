b987fa3be2996cd476f474f3c9db20773793427c
Eugene Skepner <github@skepner.eu>
2017-07-11 16:49:59 +0200

    mongo-raw

--- cc/mongo-raw.cc
+++ cc/mongo-raw.cc
@@ -17,6 +17,8 @@
 #include "acmacs-base/stream.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/iterator.hh"
+#include "acmacs-base/float.hh"
+#include "acmacs-base/json-writer.hh"
 
 // ----------------------------------------------------------------------
 
@@ -36,116 +38,240 @@ class CommandBase
 
 // ----------------------------------------------------------------------
 
-template <typename Element, typename std::enable_if<std::is_base_of<bsoncxx::v_noabi::document::element, Element>::value>::type* = nullptr>
-    std::ostream& operator << (std::ostream& out, const Element& aE);
-
-template <typename Element, typename std::enable_if<std::is_base_of<bsoncxx::v_noabi::document::element, Element>::value>::type* = nullptr>
-    inline std::string element_to_string(const Element& aE)
+template <typename Stream> inline void bson_symbol(Stream& out, const char* aSymbol)
 {
-    std::ostringstream out;
-    if (aE) {
-        switch (aE.type()) {
-          case bsoncxx::type::k_double:
-              out << aE.get_double().value;
-              break;
-          case bsoncxx::type::k_utf8:
-              out << "\"" << aE.get_utf8().value.to_string() << '"';
-              break;
-          case bsoncxx::type::k_document:
-              out << "document()";
-              break;
-          case bsoncxx::type::k_array:
-              out << '[';
-              {
-                  bool first = true;
-                  for (const auto& array_element: aE.get_array().value) {
-                      if (!first)
-                          out << ", ";
-                      else
-                          first = false;
-                      out << array_element;
-                  }
-              }
-              out << ']';
-              break;
-          case bsoncxx::type::k_binary:
-              out << "binary(size: " << aE.get_binary().size << ")";
-              break;
-          case bsoncxx::type::k_undefined:
-              out << "undefined";
-              break;
-          case bsoncxx::type::k_oid:
-              out << "ObjectId:\"" << aE.get_oid().value.to_string() << '"';
-              break;
-          case bsoncxx::type::k_bool:
-              out << aE.get_bool().value;
-              break;
-          case bsoncxx::type::k_date:
-              out << "date(?)";
-              break;
-          case bsoncxx::type::k_null:
-              out << "null";
-              break;
-          case bsoncxx::type::k_regex:
-              out << "regex(?)";
-              break;
-          case bsoncxx::type::k_dbpointer:
-              out << "dbpointer(?)";
-              break;
-          case bsoncxx::type::k_code:
-              out << "code(?)";
-              break;
-          case bsoncxx::type::k_symbol:
-              out << "symbol(?)";
-              break;
-          case bsoncxx::type::k_codewscope:
-              out << "codewscope(?)";
-              break;
-          case bsoncxx::type::k_int32:
-              out << aE.get_int32().value;
-              break;
-          case bsoncxx::type::k_timestamp:
-              out << "timestamp(?)";
-              break;
-          case bsoncxx::type::k_int64:
-              out << aE.get_int64().value;
-              break;
-          case bsoncxx::type::k_decimal128:
-              out << "decimal128(?)"; // aE.get_decimal128().value;
-              break;
-          case bsoncxx::type::k_maxkey:
-              out << "maxkey(?)";
-              break;
-          case bsoncxx::type::k_minkey:
-              out << "minkey(?)";
-              break;
-        }
-    }
-    else {
-        out << "*invalid*";
-    }
-    return out.str();
+    out << (std::string{"*"} + aSymbol + "*");
 }
 
-template <typename Element, typename std::enable_if<std::is_base_of<bsoncxx::v_noabi::document::element, Element>::value>::type*>
-    inline std::ostream& operator << (std::ostream& out, const Element& aE)
+template <typename Stream> inline Stream& operator << (Stream& out, const bsoncxx::types::b_binary& aV)
 {
-    return out << element_to_string(aE);
+    out << (std::string{"*binary(size: "} + std::to_string(aV.size) + ")*");
+    return out;
 }
+
+template <typename Stream> inline Stream& operator << (Stream& out, const bsoncxx::types::b_oid& aV)
+{
+    out << aV.value.to_string();
+    return out;
+}
+
+template <typename Stream = std::ostream> inline Stream& operator << (Stream& out, const bsoncxx::array::element& aE);
+
+inline std::ostream& operator << (std::ostream& out, const bsoncxx::array::view& aArray)
+{
+    auto first = std::begin(aArray), last = std::end(aArray);
+    out << '[';
+    for (bool insert_separator = false; first != last; ++first) {
+        if (insert_separator)
+            out << ", ";
+        else
+            insert_separator = true;
+        out << *first;
+    }
+    return out << ']';
+}
+
+template <typename Stream> inline Stream& operator << (Stream& out, const bsoncxx::types::value& aV)
+{
+    switch (aV.type()) {
+      case bsoncxx::type::k_double:
+          out << aV.get_double().value;
+          break;
+      case bsoncxx::type::k_utf8:
+          out << aV.get_utf8().value.to_string();
+          break;
+      case bsoncxx::type::k_document:
+          bson_symbol(out, "document");
+          break;
+      case bsoncxx::type::k_array:
+          out << aV.get_array().value;
+          break;
+      case bsoncxx::type::k_binary:
+          out << aV.get_binary();
+          break;
+      case bsoncxx::type::k_undefined:
+          bson_symbol(out, "undefined");
+          break;
+      case bsoncxx::type::k_oid:
+          out << aV.get_oid();
+          break;
+      case bsoncxx::type::k_bool:
+          out << aV.get_bool().value;
+          break;
+      case bsoncxx::type::k_date:
+          bson_symbol(out, "date");
+          break;
+      case bsoncxx::type::k_null:
+          bson_symbol(out, "null");
+          break;
+      case bsoncxx::type::k_regex:
+          bson_symbol(out, "regex");
+          break;
+      case bsoncxx::type::k_dbpointer:
+          bson_symbol(out, "dbpointer");
+          break;
+      case bsoncxx::type::k_code:
+          bson_symbol(out, "code");
+          break;
+      case bsoncxx::type::k_symbol:
+          bson_symbol(out, "symbol");
+          break;
+      case bsoncxx::type::k_codewscope:
+          bson_symbol(out, "codewscope");
+          break;
+      case bsoncxx::type::k_int32:
+          out << aV.get_int32().value;
+          break;
+      case bsoncxx::type::k_timestamp:
+          bson_symbol(out, "timestamp");
+          break;
+      case bsoncxx::type::k_int64:
+          out << aV.get_int64().value;
+          break;
+      case bsoncxx::type::k_decimal128:
+          bson_symbol(out, "decimal128"); // aV.get_decimal128().value;
+          break;
+      case bsoncxx::type::k_maxkey:
+          bson_symbol(out, "maxkey");
+          break;
+      case bsoncxx::type::k_minkey:
+          bson_symbol(out, "minkey");
+          break;
+    }
+    return out;
+}
+
+template <typename Stream> inline Stream& operator << (Stream& out, const bsoncxx::document::element& aE)
+{
+    if (aE)
+        out << aE.get_value();
+    else
+        bson_symbol(out, "*invalid*");
+    return out;
+}
+
+template <typename Stream> inline Stream& operator << (Stream& out, const bsoncxx::array::element& aE)
+{
+    if (aE)
+        out << aE.get_value();
+    else
+        bson_symbol(out, "*invalid*");
+    return out;
+}
+
+// inline std::string bson_value_to_string(const bsoncxx::types::value& aV)
+// {
+//     std::ostringstream out;
+//     switch (aV.type()) {
+//       case bsoncxx::type::k_double:
+//           out << aV.get_double().value;
+//           break;
+//       case bsoncxx::type::k_utf8:
+//           out << "\"" << aV.get_utf8().value.to_string() << '"';
+//           break;
+//       case bsoncxx::type::k_document:
+//           out << "document()";
+//           break;
+//       case bsoncxx::type::k_array:
+//           out << '[';
+//           {
+//               bool first = true;
+//               for (const auto& array_element: aV.get_array().value) {
+//                   if (!first)
+//                       out << ", ";
+//                   else
+//                       first = false;
+//                   out << bson_value_to_string(array_element.get_value());
+//               }
+//           }
+//           out << ']';
+//           break;
+//       case bsoncxx::type::k_binary:
+//           out << "binary(size: " << aV.get_binary().size << ")";
+//           break;
+//       case bsoncxx::type::k_undefined:
+//           out << "undefined";
+//           break;
+//       case bsoncxx::type::k_oid:
+//           out << "ObjectId:\"" << aV.get_oid().value.to_string() << '"';
+//           break;
+//       case bsoncxx::type::k_bool:
+//           out << aV.get_bool().value;
+//           break;
+//       case bsoncxx::type::k_date:
+//           out << "date(?)";
+//           break;
+//       case bsoncxx::type::k_null:
+//           out << "null";
+//           break;
+//       case bsoncxx::type::k_regex:
+//           out << "regex(?)";
+//           break;
+//       case bsoncxx::type::k_dbpointer:
+//           out << "dbpointer(?)";
+//           break;
+//       case bsoncxx::type::k_code:
+//           out << "code(?)";
+//           break;
+//       case bsoncxx::type::k_symbol:
+//           out << "symbol(?)";
+//           break;
+//       case bsoncxx::type::k_codewscope:
+//           out << "codewscope(?)";
+//           break;
+//       case bsoncxx::type::k_int32:
+//           out << aV.get_int32().value;
+//           break;
+//       case bsoncxx::type::k_timestamp:
+//           out << "timestamp(?)";
+//           break;
+//       case bsoncxx::type::k_int64:
+//           out << aV.get_int64().value;
+//           break;
+//       case bsoncxx::type::k_decimal128:
+//           out << "decimal128(?)"; // aV.get_decimal128().value;
+//           break;
+//       case bsoncxx::type::k_maxkey:
+//           out << "maxkey(?)";
+//           break;
+//       case bsoncxx::type::k_minkey:
+//           out << "minkey(?)";
+//           break;
+//     }
+//     return out.str();
+// }
+
+// inline std::string element_to_string(const bsoncxx::v_noabi::document::element& aE)
+// {
+//     return aE ? bson_value_to_string(aE.get_value()) : std::string{"*invalid*"};
+// }
+
+// inline std::string element_to_string(const bsoncxx::v_noabi::array::element& aE)
+// {
+//     return aE ? bson_value_to_string(aE.get_value()) : std::string{"*invalid*"};
+// }
+
+// ----------------------------------------------------------------------
 
 class DocumentFindResults
 {
  public:
+    using value_type = bsoncxx::v_noabi::types::value;
+
     inline DocumentFindResults() {}
-    inline DocumentFindResults(mongocxx::v_noabi::cursor&& aCursor) { build(std::move(aCursor)); }
+    inline DocumentFindResults(mongocxx::v_noabi::cursor&& aCursor, const std::vector<std::string>& aExcludeFields )
+        : mExcludeFields(std::begin(aExcludeFields), std::end(aExcludeFields)) { build(std::move(aCursor)); }
 
     void build(mongocxx::v_noabi::cursor&& aCursor)
         {
             for (auto record: aCursor) {
-                std::map<std::string, std::string> aRecord;
+                std::map<std::string, value_type> aRecord;
                 for (auto field: record) {
-                    mFields.insert(field.key().to_string());
-                    aRecord.emplace(field.key().to_string(), element_to_string(field));
+                    const auto field_name = field.key().to_string();
+                    if (mExcludeFields.find(field_name) == mExcludeFields.end()) {
+                        mFields.insert(field_name);
+                        aRecord.emplace(field.key().to_string(), field.get_value());
+                    }
                 }
                 mRecords.push_back(aRecord);
             }
@@ -154,13 +280,13 @@ class DocumentFindResults
     inline std::string csv() const
         {
             std::ostringstream result;
-            std::transform(std::begin(mFields), std::end(mFields), std::make_ostream_joiner(result, ","), &DocumentFindResults::to_csv);
+            std::transform(std::begin(mFields), std::end(mFields), std::make_ostream_joiner(result, ","), &DocumentFindResults::string_to_csv);
             result << '\n';
-            auto put_record = [this](const std::map<std::string, std::string>& aRec) {
+            auto put_record = [this](const std::map<std::string, value_type>& aRec) {
                 std::ostringstream out_record;;
                 auto put_field = [&aRec](std::string aField) -> std::string {
                     auto f = aRec.find(aField);
-                    return f == aRec.end() ? std::string{} : DocumentFindResults::to_csv(f->second);
+                    return f == aRec.end() ? std::string{} : DocumentFindResults::value_type_to_csv(f->second);
                 };
                 std::transform(std::begin(this->mFields), std::end(this->mFields), std::make_ostream_joiner(out_record, ","), put_field);
                 return out_record.str();
@@ -170,10 +296,18 @@ class DocumentFindResults
         }
 
  private:
+    std::set<std::string> mExcludeFields;
     std::set<std::string> mFields;
-    std::vector<std::map<std::string, std::string>> mRecords;
+    std::vector<std::map<std::string, value_type>> mRecords;
 
-    inline static std::string to_csv(std::string src)
+    inline static std::string value_type_to_csv(value_type src)
+        {
+            std::ostringstream out;
+            out << src;
+            return "\"" + string::replace(out.str(), "\"", "\"\"") + "\"";
+        }
+
+    inline static std::string string_to_csv(std::string src)
         {
             return "\"" + string::replace(src, "\"", "\"\"") + "\"";
         }
@@ -200,7 +334,10 @@ class CommandUsers : public CommandBase
  public:
     virtual void process(mongocxx::database& aDb)
         {
-            DocumentFindResults results{aDb["users_groups"].find(Filter{} << "_t" << "acmacs.mongodb_collections.users_groups.User" << Fin)};
+            DocumentFindResults results{
+                aDb["users_groups"].find(Filter{} << "_t" << "acmacs.mongodb_collections.users_groups.User" << Fin),
+                {"_id", "_t", "password", "nonce"}
+            };
             std::cout << results.csv() << std::endl;
             // for (auto doc: aDb["users_groups"].find(Filter{} << "_t" << "acmacs.mongodb_collections.users_groups.User" << Fin)) {
             //     // std::cout << bsoncxx::to_json(doc) << std::endl;
