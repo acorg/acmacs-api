04421b47c536a125a22beb3aa939d79c4ab8a9f7
Eugene Skepner <github@skepner.eu>
2017-07-15 13:53:15 +0200

    acmacs-api-client

--- client/acmacs-api-client.cc
+++ client/acmacs-api-client.cc
@@ -5,7 +5,9 @@
 
 namespace client
 {
+    Array* jopa(Object* obj);
     Array* jopa(Object& obj);
+
 }
 
 
@@ -25,18 +27,24 @@ void webMain()
     auto* ws = new WebSocket("wss://localhost:1169/api");
     ws->set_onmessage(cheerp::Callback(on_message));
 
-    __asm__("window.jopa = function(obj) { return Object.keys(obj); };");
+      //__asm__("window.jopa = function(obj) { return Object.keys(obj); };");
+    __asm__("window.jopa = Object.keys;");
 }
 
 // ----------------------------------------------------------------------
+
+inline Object* get_value(Object* obj, String&& key)
+{
+    return (*obj)[key];
+}
 
 void on_message(MessageEvent* aEvent)
 {
     console.log("on_message", aEvent->get_origin());
     console.log(JSON.stringify(aEvent->get_data()));
-    auto& message = *JSON.parse(static_cast<String*>(aEvent->get_data()));
+    auto message = JSON.parse(static_cast<String*>(aEvent->get_data()));
     console.log("keys", JSON.stringify(jopa(message)));
-    auto version = static_cast<String*>(message[String{"hello"}]);
+    auto version = static_cast<String*>(get_value(message, "hello"));
     console.log(version, version == new String{"acmacs-api-server-v1"});
 
 } // on_message
