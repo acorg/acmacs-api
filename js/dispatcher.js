8587e9c56dee8b526b17ccefc5efbf5a0b502f57
Eugene Skepner <github@skepner.eu>
2018-04-27 18:18:01 +0200

    api app development

--- js/dispatcher.js
+++ js/dispatcher.js
@@ -134,12 +134,63 @@ export class Dispatcher {
 
 // ----------------------------------------------------------------------
 
+const LoginWidget_html = "\
+<div class='login box-shadow-popup' id='login'>\
+  <div class='title'>Acmacs-Web</div>\
+  <form>\
+    <div name='username-label'>Username</div>\
+    <input autocomplete=username spellcheck=false tabIndex=1 name=username type=email></input>\
+    <div class='separator' name=username></div>\
+    <div name='password-label'>Password</div>\
+    <input autocomplete=password spellcheck=false tabIndex=2 name=password type=password></input>\
+    <div class='separator' name=password></div>\
+    <div class='button box-shadow-button'>Login</div>\
+    <div class='error-message'></div>\
+  </form>\
+</div>\
+";
+
+const LoginWidget_css = "\
+<style name='login-widget'>\
+  #login.box-shadow-popup { box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14),0 3px 1px -2px rgba(0,0,0,0.12),0 1px 5px 0 rgba(0,0,0,0.2); }\
+  #login .box-shadow-button {\
+    border-radius: 3px;\
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14),0 3px 1px -2px rgba(0,0,0,0.12),0 1px 5px 0 rgba(0,0,0,0.2);\
+    transition: box-shadow .28s cubic-bezier(0.4,0.0,0.2,1);\
+  }\
+\
+\
+  #login { width: 20em; height: 20em; padding: 2em;\
+          position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto;}\
+  #login .title { font: 32px Helvetica,Arial,sans-serif; font-weight: bold; margin-bottom: 2em; color: $aw-orange; }\
+  #login input { border: none; width: 100%; font: 400 16px Helvetica,Arial,sans-serif; }\
+  #login input:focus { outline: none; }\
+  #login div.separator { background-color: rgba(0,0,0,0.12); height: 1px; width: 100%; margin-bottom: 2em };\
+  #login div.separator-focused { background-color: #54964e; height: 2px; transition: 0.2s; }\
+  #login div.label-focused { color: #54964e; transition: 0.2s; }\
+  #login .button { margin-left: auto; width: 4em; background-color: #54964e; color: white; font-size: 1.2em; padding: 0.2em 0.2em; text-align: center; font-weight: bold; cursor: pointer; }\
+  #login .error-message { color: red; margin-top: 1em; }\
+</style>\
+";
+
 class LoginWidget {
+
     constructor(dispatcher) {
-        this.div = $("<div class='login box-shadow-popup hidden' id='login'><div class='title'>Acmacs-Web</div><form><div>Username</div></form></div>").appendTo($("body"));
+        if ($("head").find("style[name='login-widget']").length === 0)
+            $("head").append(LoginWidget_css);
+        this.div = $(LoginWidget_html).appendTo($("body"));
+        let username_input = this.div.find("input[name=username]");
+        let username_separator = this.div.find("div.separator[name=username]");
+        let username_label = this.div.find("div[name='username-label']");
+        username_input.on("focus", () => {
+            username_input.select();
+            username_separator.addClass("separator-focused");
+            username_label.addClass("label-focused");
+        });
     }
 
     destroy() {
+        this.div.remove();
     }
 }
 
