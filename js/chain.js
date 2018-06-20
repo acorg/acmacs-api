ed14fc2aeaa999a3527ed9b071dd15c5e6f95e16
Eugene Skepner <github@skepner.eu>
2018-06-20 11:12:58 +0200

    setting size of the window showing hidb point info

--- js/chain.js
+++ js/chain.js
@@ -610,8 +610,12 @@ function show_point_info(dispatcher, point, invoking_node) {
 
     const make_window = (message, invoking_node, title, point, content_filler) => {
         // console.log("show_point_info", message);
+        const max_win_content_height = 300;
         const win = new acv_toolkit.MovableWindow({title: title, parent: invoking_node, classes: "hidb-point-info", content_css: {width: "auto", height: "30em"}});
         content_filler(message, point, win.content());
+        win.content().css("height", "auto");
+        if (win.content().height() > max_win_content_height)
+            win.content().css("height", "" + max_win_content_height + "px");
     };
 
     if (point.antigen) {
