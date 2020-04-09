b5d5cb23ddbacc74d6ff9f166c4e5301157250c8
Eugene Skepner <github@skepner.eu>
2020-04-09 11:20:31 +0200

    acmacs::string::join

--- cc/acmacs-c2.cc
+++ cc/acmacs-c2.cc
@@ -6,6 +6,7 @@
 #include <curl/curl.h>
 
 #include "acmacs-base/range.hh"
+#include "acmacs-base/string-join.hh"
 #include "acmacs-base/string.hh"
 
 #include "acmacs-api/curl.hh"
@@ -39,7 +40,7 @@ std::string AcmacsC2::embed_session_in_command(const SessionId& aSession, std::s
 
 std::string AcmacsC2::ace_uncompressed(const SessionId& aSession, std::string aObjectId, size_t aMaxNumberOfProjections)
 {
-    const auto projections = aMaxNumberOfProjections == static_cast<size_t>(-1) ? std::string{} : ",\"projection\":[" + string::join(",", acmacs::index_iterator(0UL), acmacs::index_iterator(aMaxNumberOfProjections)) + "]";
+    const auto projections = aMaxNumberOfProjections == static_cast<size_t>(-1) ? std::string{} : ",\"projection\":[" + acmacs::string::join(",", acmacs::index_iterator(0UL), acmacs::index_iterator(aMaxNumberOfProjections)) + "]";
     auto result = command(aSession, std::string{R"({"C":"chart_export","format":"ace_uncompressed","pretty":false,"id":")"} + aObjectId + '"' + projections + "}");
       // "chart_json" is a string with embedded json, all double-quotes are escaped
     return string::replace(rjson::get_or(result, "chart_json", "* C2 chart_export failed without error *"), "\\\"", "\"");
